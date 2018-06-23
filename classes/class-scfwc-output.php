4895aebd47cf5f6b241f04630aea15b8af4a0a0c
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-23 23:13:51 +1000

    [ ADDED ] html output and render location

--- classes/class-scfwc-output.php
+++ classes/class-scfwc-output.php
@@ -27,16 +27,59 @@ class scfwc_output {
 	 * @since 1.0
 	 */
 	function scfwc_output_add_actions_filters() {
-		add_action( 'ACTION REFERENCE HOOK', array( $this, 'scfwc_output' ) );
+		//add_action( 'ACTION REFERENCE HOOK', array( $this, 'scfwc_output_js' ), 999 );
+
+		$scfwc_render_location = get_theme_mod( 'scfwc_render_location');
+		switch ( $scfwc_render_location ) :
+			case 'scfwc_after_heading' :
+				add_action( 'woocommerce_single_product_summary', array( $this, 'scfwc_html_product' ), 6 );
+				break;
+			case 'scfwc_after_price' :
+				add_action( 'woocommerce_single_product_summary', array( $this, 'scfwc_html_product' ), 11 );
+				break;
+			case 'pffwc_after_short_desc' :
+				add_action( 'woocommerce_single_product_summary', array( $this, 'scfwc_html_product' ), 21 );
+				break;
+			case 'pffwc_after_add_cart' :
+				add_action( 'woocommerce_single_product_summary', array( $this, 'scfwc_html_product' ), 31 );
+				break;
+		endswitch;
 	}
 
   /**
-   * WHAT MAGIC WILL HAPPEN
+   * Calculate the time in JS
    * @since 1.0
    */
-  function scfwc_output() {
-		// let the magic happen here
+  function scfwc_output_js() { ?>
+		<script type="text/javascript"></script> <?php
 	}
+
+	/**
+	 * Add html to prodcut page for binding the countdown
+	 * @since 1.0
+	 */
+	function scfwc_html_product() { ?>
+			<div id="shipping-countdown">
+				<h1>Shipping Location</h1>
+			  <div>
+			    <span class="days"></span>
+			    <div class="smalltext">Days</div>
+			  </div>
+			  <div>
+			    <span class="hours"></span>
+			    <div class="smalltext">Hours</div>
+			  </div>
+			  <div>
+			    <span class="minutes"></span>
+			    <div class="smalltext">Minutes</div>
+			  </div>
+			  <div>
+			    <span class="seconds"></span>
+			    <div class="smalltext">Seconds</div>
+			  </div>
+			</div> <?php
+	}
+
 
 } // END class
 
