8afc0573def16050a014e26888de4f6c5bc6779a
Bradley Davis <Bradley-D@users.noreply.github.com>
2018-06-23 18:05:08 +1000

    [ ADDED ] customizer control multi select

--- classes/class-scfwc-customizer.php
+++ classes/class-scfwc-customizer.php
@@ -27,21 +27,137 @@ class scfwc_customizer {
 	 * @since 1.0
 	 */
 	public static function scfwc_customizer_add_actions_filters() {
-		add_action( 'ACTION REFERENCE HOOK', __CLASS__ . '::scfwc_customizer_add_options' );
+    //self::scfwc_includes();
+		add_action( 'customize_register', __CLASS__ . '::scfwc_customizer_add_options' );
 	}
+
+  /**
+   * Add the includes.
+   * @since 1.0
+   */
+  public static function scfwc_includes() {
+  //  require_once( 'class_extends_customizer.php');
+  }
 
   /**
    * WHAT MAGIC WILL HAPPEN
    * @since 1.0
    */
-  public static function scfwc_customizer_add_options() {
+  public static function scfwc_customizer_add_options( $wp_customize ) {
+
+    require_once( 'class_extends_customizer.php');
+
 		// let the magic happen here
     // NEED:
-    // - add to WooCommerce pannel - title Shipping Countdown
+
     // - select with days of week for shipping
     // - - need to have multi select
     // - add time to select shipping countdown
+
+    // Add section to WooCommerce Panel
+    $wp_customize->add_section( 'scfwc_options',
+      array(
+        'title'      => __( 'Shipping Countdown', 'scfwc' ),
+        'panel'      => 'woocommerce',
+        'type'       => 'option',
+        'capability' => '',
+        'priority'   => 500,
+      )
+    );
+
+    // Choose Shipping Countdown days of shipping
+    $wp_customize->add_setting( 'scfwc_select_days',
+      array(
+        'default' => 'scfwc_ship_fri',
+        'sanitize_callback' => __CLASS__ . '::scfwc_sanitize_day',
+      )
+    );
+    $wp_customize->add_control( new scfwc_dropdown_custom_control( $wp_customize, 'scfwc_select_days',
+      array(
+        'label'       => __( 'Select shipping days', 'scfwc' ),
+        'description' => __( 'Choose the days that you ship products.', 'scfwc'),
+        'settings'    => 'scfwc_select_days',
+        'section'     => 'scfwc_options',
+        'type'        => 'multiple-select',
+        'choices'     => array(
+          'scfwc_ship_mon' => __( 'Monday', 'scfwc' ),
+          'scfwc_ship_tue' => __( 'Tuesday', 'scfwc' ),
+          'scfwc_ship_wed' => __( 'Wednesday', 'scfwc' ),
+          'scfwc_ship_thu' => __( 'Thursday', 'scfwc' ),
+          'scfwc_ship_fri' => __( 'Friday', 'scfwc' ),
+          'scfwc_ship_sat' => __( 'Saturday', 'scfwc' ),
+          'scfwc_ship_sun' => __( 'Sunday', 'scfwc' ),
+        ),
+      )
+    ));
+
+
+    // Choose Shipping Countdown output location
+    $wp_customize->add_setting( 'scfwc_render_location',
+      array(
+        'default' => 'scfwc_after_add_cart',
+        'sanitize_callback' => __CLASS__ . '::scfwc_sanitize_location',
+      )
+    );
+    $wp_customize->add_control( 'scfwc_render_location',
+      array(
+        'label'    => __( 'Add shipping countdown to:', 'scfwc' ),
+        'description' => __( 'Choose the location that you would like the shipping countdown to display.', 'scfwc'),
+        'settings' => 'scfwc_render_location',
+        'section'  => 'scfwc_options',
+        'type'     => 'select',
+        'choices'  => array(
+          'scfwc_after_heading'      => __( 'After product heading', 'scfwc' ),
+          'scfwc_after_price'        => __( 'After product price', 'scfwc' ),
+          'scfwc_after_short_desc'   => __( 'After short description', 'scfwc' ),
+          'scfwc_after_add_cart'     => __( 'After add to cart', 'scfwc' ),
+        ),
+      )
+    );
 	}
+
+  /**
+   * Check the shipping day option is real
+   * @return string $input
+   * @since 1.0
+   */
+  public static function scfwc_sanitize_day( $input ) {
+    $scfwc_valid_day = array(
+      'scfwc_ship_mon' => 'Monday',
+      'scfwc_ship_tue' => 'Tuesday',
+      'scfwc_ship_wed' => 'Wednesday',
+      'scfwc_ship_thu' => 'Thursday',
+      'scfwc_ship_fri' => 'Friday',
+      'scfwc_ship_sat' => 'Saturday',
+      'scfwc_ship_sun' => 'Sunday',
+    );
+
+    if ( array_key_exists( $input, $scfwc_valid_day ) ) :
+      return $input;
+    else :
+      return '';
+    endif;
+  }
+
+  /**
+   * Check the render locatoin option is real
+   * @return string $input
+   * @since 1.0
+   */
+  public static function scfwc_sanitize_location( $input ) {
+    $scfwc_valid_location = array(
+      'scfwc_after_heading'      => 'After product heading',
+      'scfwc_after_price'        => 'After product price',
+      'scfwc_after_short_desc'   => 'After short description',
+      'scfwc_after_add_cart'     => 'After add to cart',
+    );
+
+    if ( array_key_exists( $input, $scfwc_valid_location ) ) :
+      return $input;
+    else :
+      return '';
+    endif;
+  }
 
 } // END class
 
---
+++ classes/class_extends_customizer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Class to create a custom multiselect dropdown control
+*/
+class scfwc_dropdown_custom_control extends WP_Customize_Control {
+  /**
+  * Render the content on the theme customizer page
+  */
+  public $type = 'multiple-select';
+  //scfwc_render_multiselect
+  public function render_content() { ?>
+    <label>
+      <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
+      <span class="description customize-control-description"><?php echo esc_html( $this->description ); ?></span>
+      <select <?php $this->link(); ?> multiple="multiple" size="7"> <?php
+
+        foreach ( $this->choices as $value => $label ) :
+          $selected = ( in_array( $value, $this->value() ) ) ? selected( 1, 1, false ) : '';
+          echo '<option value="' . esc_attr( $value ) . ' " ' . esc_attr( $selected ) . '>' . esc_attr( $label ) . '</option>';
+        endforeach; ?>
+
+      </select>
+    </label> <?php
+  }
+}
